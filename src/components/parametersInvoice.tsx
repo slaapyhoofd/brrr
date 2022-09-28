ed75bf3f32ba28566c4e76746a8fdf5e84551a16
arbadacarba <63317640+arbadacarbaYK@users.noreply.github.com>
2022-09-29 00:57:57 +0200

    Update parametersInvoice.tsx

--- src/components/parametersInvoice.tsx
+++ src/components/parametersInvoice.tsx
@@ -20,27 +20,20 @@ export const ParametersInvoice = ({
   const { amount, memo, webhook } = parameters;
   return (
     <article>
-      <header>Parameters for filling up the cards with sats</header>
+      <header>Parameter for funding your cards with sats</header>
       <Field
         id="amount"
-        label="How much sat do you want put on the card"
+        label="Amount of sats per card"
         value={amount + ''}
         type="number"
         onChange={(v) => updateParameters({ ...parameters, amount: safeParseInt(v, 1) })}
       />
       <Field
         id="memo"
-        label="Leave a fill-up message for the user"
+        label="Message for funding transaction"
         required={false}
         value={memo || ''}
         onChange={(v) => updateParameters({ ...parameters, memo: v })}
-      />
-      <Field
-        id="webhook_url"
-        label="Url to trigger to after invoice is paid"
-        required={false}
-        value={webhook || ''}
-        onChange={(v) => updateParameters({ ...parameters, webhook: v })}
       />
     </article>
   );
